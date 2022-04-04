b2c48528e7db529ddfc110c28c799d71cb635f51
Chase Martell <74631685+ChaseMartell@users.noreply.github.com>
2022-04-03 21:29:08 -0600

    Update app_controller.js

--- js/controllers/app_controller.js
+++ js/controllers/app_controller.js
@@ -1,27 +1,26 @@
-class AppController
+import LocalStorageService from '../models/local_storage_service.js'
+import ListView from '../views/list_view.js'
+
+export default class AppController
 {
    constructor(appViewModel)
    {
         this.appViewModel = appViewModel;
 
-        this.storageService = new LocalStorageService(this.data, this.entity,this.list.options);
+        this.storageService = new LocalStorageService(this.data, this.entity, this.entitySingle, this.options);
 
-        this._view = new ListPageView(this.storageService, this.listViewModel)
+        this.view = new ListView(this.storageService, this.appViewModel.viewModel);
        
     }
     get data(){return this.appViewModel.viewModel.data;}
     get entity(){return this.appViewModel.viewModel.entity;}
-    get list(){ return this.appViewModel.viewModel.list;}
-    get listViewModel(){return this.appViewModel.viewModel;}
-
-    get view(){
-        return this._view;
-    }
+    get entitySingle(){return this.appViewModel.viewModel.entitySingle;}
+    get options() {return this.appViewModel.viewModel.list.options;}
+    
     async reset(){
         await this.view.reset();
     }
     async render(){
        await this.view.render();
     }
-   
 }
