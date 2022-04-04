3a9fdeaed219111fa319751508a6f520e4824395
Chase Martell <74631685+ChaseMartell@users.noreply.github.com>
2022-04-03 21:30:38 -0600

    Create mock_team_data.js

---
+++ js/models/mock/mock_team_data.js
@@ -0,0 +1,34 @@
+let mockTeamData=[
+    {
+      "id": 0,
+      "name":"Fire Squad",
+      "trainer": "Barry B.",
+      "rating": "90",
+    },
+    {
+      id: 1,
+      "name":"The Chads",
+      "trainer": "Chad M.",
+      "rating": "85",
+    },
+    {
+      id: 2,
+      "name":"Team Rumble",
+      "trainer": "Dylan K.",
+      "rating": "79",
+    },
+    {
+      id: 3,
+      "name":"Poke Gang",
+      "trainer": "Ash K.",
+      "rating": "94",
+    },
+    {
+      id: 4,
+      "name":"Smokin' ",
+      "trainer": "Grant J.",
+      "rating": "81",
+    }
+  ]
+
+export default mockTeamData;
